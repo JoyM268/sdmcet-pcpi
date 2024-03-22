716a15ac01a81f18894ae62771878bdd3ade0b37
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-03-22 19:45:41 +0530

    Lab - 3

--- swap_xor.s
+++ swap_xor.s
@@ -2,6 +2,7 @@
 ;NUM1 is loaded into R0
 ;NUM2 is loaded into R1
 ;Finally the values in R0 and R1 are swapped
+
 	AREA swap_xor, CODE, READONLY
 	MOV R0, #06	;Move immediate value 06 into register R0
 	MOV R1, #08	;Move immediate value 08 into register R1
