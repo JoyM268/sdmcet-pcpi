fccf45a0b8b4719a79ddc06dfc8678e26e953b99
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-03-22 12:03:03 +0530

    Lab - 3

--- swap_xor.s
+++ swap_xor.s
@@ -1,8 +1,8 @@
-;Program to swap register contents without temporary register
+;Program to swap register contents without temporary register using XOR
 ;NUM1 is loaded into R0
 ;NUM2 is loaded into R1
 ;Finally the values in R0 and R1 are swapped
-	AREA swap32, CODE, READONLY
+	AREA swap_xor, CODE, READONLY
 	MOV R0, #06	;Move immediate value 06 into register R0
 	MOV R1, #08	;Move immediate value 08 into register R1
 	EOR R0, R0, R1	;XOR R0 with R1 and store result in R0
