ad255cccdbd7483effbb803bda9f765abd378343
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-04-04 20:07:14 +0530

    Lab - 5

--- array_sum.s
+++ array_sum.s
@@ -2,7 +2,7 @@
 	AREA array_sum, CODE, READONLY
 	LDR R0, =ARR1	;Load memory address of ARR1 into R0
 	MOV R1, #0	;Initialize sum(R1) with 0 
-	MOV	R2, #05	;Store the number of elements(5) int R2
+	MOV	R2, #05	;Store the number of elements(5) into R2
 
 nextele LDR R3, [R0], #04	;Load the value stored at memory address in R0 into R3 and increment R0 by 4
 	ADDS R1, R1, R3	;Add the value in R3 aand R1 and store in R1
