ff69946da7e9d5932ea9eb5a073b11292a8595d1
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-05-02 23:57:28 +0530

    Add File

---
+++ IMP Programs/square.s
@@ -0,0 +1,13 @@
+;Program to find square of a number
+	AREA square, CODE, READONLY
+	LDR R0, =NUM1
+	LDR R9, =RESULT
+	LDR R1, [R0]
+	MUL R2, R1, R1
+	STR R2, [R9]
+	NOP
+	NOP
+	AREA data1, DATA, READWRITE
+RESULT DCD 0x0
+NUM1 DCD 0x0
+	END
