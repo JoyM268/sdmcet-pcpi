99cb51b5c299737be217b51e76a46132b88b0190
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-05-02 19:23:21 +0530

    Add File

---
+++ IMP Programs/add16.s
@@ -0,0 +1,16 @@
+;Program to add two 16-bit numbers
+	AREA add16, CODE, READONLY
+	LDR R0, =NUM1
+	LDR R1, =NUM2
+	LDR R9, =RESULT
+	LDRH R2, [R0]
+	LDRH R3, [R1]
+	ADD R4, R3, R2
+	STRH R4, [R9]
+	NOP
+	NOP
+	AREA data1, DATA, READWRITE
+RESULT DCW 0x0
+NUM1 DCW 0x0
+NUM2 DCW 0x0
+	END
---
+++ IMP Programs/add8.s
@@ -0,0 +1,16 @@
+;Program to add two 8-bit numbers
+	AREA add8, CODE, READONLY
+	LDR R0, =NUM1
+	LDR R1, =NUM2
+	LDR R9, =RESULT
+	LDRB R2, [R0]
+	LDRB R3, [R1]
+	ADD R4, R2, R3
+	STRB R4, [R9]
+	NOP
+	NOP
+	AREA data1, DATA, READWRITE
+RESULT DCB 0x0
+NUM1 DCB 0x0
+NUM2 DCB 0x0
+	END
