bbbaef06ffe79e4d1f2c4288512f80327377e2ec
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-05-03 20:51:20 +0530

    Add File

--- IMP Programs/square.s
+++ IMP Programs/square.s
@@ -3,8 +3,19 @@
 	LDR R0, =NUM1
 	LDR R9, =RESULT
 	LDR R1, [R0]
-	MUL R2, R1, R1
-	STR R2, [R9]
+	
+	;MUL R2, R1, R1
+	;STR R2, [R9]
+	
+	MOV R2, #0
+	MOV R3, #0
+
+next ADD R3, R3, R1
+	ADD R2, R2, #01
+	CMP R2, R1
+	BNE next
+
+	STR R3, [R9]
 	NOP
 	NOP
 	AREA data1, DATA, READWRITE
