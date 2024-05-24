61173cb38bd134b7328015944186cefda76691d2
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-05-24 11:07:19 +0530

    Add File

---
+++ Lab Programs/string_length.s
@@ -0,0 +1,14 @@
+;Program to find length of a given string
+	AREA string_length, CODE, READONLY
+	MOV R0, #0
+	LDR R1, =STR
+nextchar LDRB R2, [R1], #01
+	CMP R2, #0
+	BEQ STOP
+	ADD R0, R0, #01
+	B nextchar
+STOP
+	NOP
+	NOP
+STR DCB "Hello", 0
+	END
